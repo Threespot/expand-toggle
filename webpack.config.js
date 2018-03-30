4e7026c8b5cb99baadf390ae7663596cf39941d9
Ted Whitehead <ted.whitehead@threespot.com>
2018-03-30 10:41:56 -0400

    Add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,15 @@ const defaultConfig = {
   module: {
     rules: [
       {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          failOnError: true,
+        }
+      },
+      {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
