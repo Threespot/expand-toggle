66bcfed99235f3a1be0b8a8223d55461b1788073
Ted Whitehead <tedwhitehead@gmail.com>
2020-09-04 15:06:45 -0400

    chore: update dependencies, bump minimum node version

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 
